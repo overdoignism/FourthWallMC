737d2208cfa2c329e100f957f4b4482315e1a09c
overdoignism <70434709+overdoignism@users.noreply.github.com>
2021-02-27 21:18:55 +0800

    First Upload
    First Upload

---
+++ PowerShell files/BackupServerSample.ps1
@@ -0,0 +1,83 @@
+﻿#
+# It's a sample code for FourthWallMC, use to Broadcast, then stop server, backup server, and restart server. 
+#
+# 2021-02-27
+#
+# Please change the port to your real FourthWallMC management port.
+#
+
+$GCMC_Port = 25566
+
+#
+# Please change the IP to your real server IP. (If in same machine, just keep 127.0.0.1)
+#
+
+$GCMC_IP = '127.0.0.1'
+
+#
+# Please change the password in to your real password.
+#
+
+$GCMC_PWD = 'password'
+
+# Import Connect command.
+. .\ConnectServer.ps1
+
+
+#
+#Syntax: ConnectServer -port (port) -server (IP or Domainname) -password(Password) -message (See command document) 
+#
+#        -waitsec (Wait seconds for Busy) -err2stop (If error happend stop) -showResult (Show the return form server)
+#
+
+#Program begin.
+Write-Host "Backup task started."
+
+# Test for if Minecraft server is on.
+$result = ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'cm,info1' -waitsec 20 -err2stop $true -showResult $true
+
+if ($result -eq "ON") 
+{
+
+    #Broadcast
+    Write-Host "Countdown 5 min."
+    $result = ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'in,say We will shutdown server in 5 min.' -waitsec 0 -err2stop $true -showResult $true
+    Start-Sleep -second 120
+
+    #Broadcast
+    Write-Host "Countdown 3 min."
+    $result = ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'in,say We will shutdown server in 3 min.' -waitsec 0 -err2stop $true -showResult $true
+    Start-Sleep -second 120
+
+    #Broadcast
+    Write-Host "Countdown 1 min."
+    $result =  ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'in,say We will shutdown server in 1 min.' -waitsec 0 -err2stop $true -showResult $true
+    Start-Sleep -second 60
+
+    #Broadcast
+    Write-Host "Countdown end, we are shutting down."
+    $result =  ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'in,say We are shutting down the server.' -waitsec 0 -err2stop $true -showResult $true
+    
+    #Shutdown Minecraft
+    Write-Host "Minecraft is Shutdown now."
+    $result =  ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'in,stop' -waitsec 0 -err2stop $true -showResult $true
+
+    #Backup Minecraft
+    Write-Host "Start Backup.."
+    $result =  ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'cm,backup' -waitsec 120 -err2stop $true -showResult $true
+
+    #Start Minecraft
+    Write-Host "Restart Minecraft Server....."
+    $result =  ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'cm,start' -waitsec 120 -err2stop $true -showResult $true
+
+}
+elseif ($result -eq "OFF")
+{
+    #Backup Minecraft
+    Write-Host "Start Backup.."
+    $result =  ConnectServer -port $GCMC_Port -server $GCMC_IP -password $GCMC_PWD -message 'cm,backup' -waitsec 120 -err2stop $true -showResult $true
+}
+else
+{
+    write-host 'FourthWallMC may not working, or setting/password incorrect!'
+}
---
+++ PowerShell files/ConnectServer.ps1
@@ -0,0 +1,117 @@
+﻿#
+# It's a sample code for FourthWallMC
+#
+# 2021-02-27
+#
+# Please do not modify this if you are not know what are you doing.
+#
+
+function ServerSocket {	
+	param ($socket_message, $socket_server, $socket_port)
+
+	Start-Sleep -Milliseconds 50
+	$ReturnVaule = "FAIL"
+
+	try {
+
+		$socket = new-object System.Net.Sockets.TcpClient($socket_server, $socket_port)
+		$socket.ReceiveTimeout = 10000
+		$socketNetworkStream = $Socket.GetStream()
+
+		if ($socket.Connected)
+		{
+
+			if ($socketNetworkStream.CanWrite)
+			{
+				$socketWriteByte = [System.Text.Encoding]::UTF8.GetBytes($socket_message)
+				$socketNetworkStream.Write($socketWriteByte, 0, $socketWriteByte.Length)
+				$socketNetworkStream.Flush()
+			}
+
+	    		$Receivebuffer = New-Object Byte[] $socket.ReceiveBufferSize
+
+			if ($socketNetworkStream.CanRead)
+			{
+				$ReadLength = $socketNetworkStream.Read($Receivebuffer, 0, $socket.ReceiveBufferSize) 
+				$ReturnVaule = [System.Text.Encoding]::UTF8.GetString($Receivebuffer)
+				$ReturnVaule = $ReturnVaule.Trim([char]0x0000)
+			}
+		}
+	}
+	catch
+	{
+	}
+
+	return $ReturnVaule
+}
+
+
+function ConnectServer {
+	param ($server, $port, $password, $message, $waitsec, $err2stop = $true, $showResult = $false)
+
+	$ReturnResult = 'BUSY'
+	$WaitCount = 0
+	$UseString = $password + "," + $message
+
+	while($ReturnResult -eq 'BUSY')
+	{
+
+		try
+		{
+			$ReturnResult = ServerSocket -socket_server $server -socket_port $port -socket_message $UseString
+			if($showResult -eq $true) { Write-Host "Socket reutrn:"$ReturnResult }
+			Start-Sleep -s 1
+		}
+		catch
+		{
+			$ReturnResult='FAIL'
+		}
+
+		if($waitsec -gt 0) {$WaitCount += 1}
+
+		if ($WaitCount -gt $waitsec)
+		{
+			Write-Host 'The task has timed out.'
+			if ($err2stop) {exit}
+		}
+		else
+		{
+			
+			switch ($ReturnResult)
+			{
+				'PASS'
+				{
+					$UseString = $password + ",sy,This task has been cancelled."
+					$NoDisplay = ServerSocket -socket_server $server -socket_port $port -socket_message $UseString
+					Write-Host "PASS: This task has been cancelled."
+                    			Exit
+ 				}
+
+	 			'NOT-ON'
+   			        {
+	        		        Write-Host 'NOT-ON: This operation is need Minecraft server be on-line.'
+ 					if ($err2stop) {exit}
+                		}
+
+	    			'NOT-OFF'
+				{
+					Write-Host 'NOT-OFF: This operation is need Minecraft server be off-line.'
+					if ($err2stop) {exit}
+				}
+
+				'FAIL'
+                		{	
+					Write-Host 'Error happend during connection.'
+					if ($err2stop) {exit}
+                		}
+
+				default
+				{
+    				}
+			}
+		}
+	}
+
+	return $ReturnResult
+
+}
