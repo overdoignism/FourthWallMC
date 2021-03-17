2c7664b006ec97c2483ecd2c0db571bf93eb0119
overdoignism <70434709+overdoignism@users.noreply.github.com>
2021-03-18 03:50:03 +0800

    Up-to-date
    If you are first time use powershell, run it as admin. (1 of 4in1.zip)

---
+++ PowerShell files/FirstTime.bat
@@ -0,0 +1,15 @@
+@ECHO off
+Echo ************************************************************
+Echo  It's a simple tool to change PowerShell Execution Policy.
+Echo ************************************************************
+Echo.
+Echo.
+Powershell -Command Set-ExecutionPolicy RemoteSigned CurrentUser
+Powershell -Command Get-ExecutionPolicy -list
+Echo.
+Echo.
+Echo If you see "CurrentUser RemoteSigned", It's work successfuly.
+Echo.
+cd %~dp0
+
+Powershell -NoExit -Command Set-Location -LiteralPath .
